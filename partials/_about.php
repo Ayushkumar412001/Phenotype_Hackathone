586769576a49b932f985791aa54362de98e83342
Ayush Kumar <ayushkumar412001@gmail.com>
2023-12-03 18:41:57 +0530

    done

--- partials/_about.php
+++ partials/_about.php
@@ -9,7 +9,7 @@
         <h1>What's your Prakriti?</h1>
         <p>A phenotype refers to the observable characteristics or traits of an organism, resulting from the interaction between its genetic makeup (genotype) and the environment. These traits can include physical features, behavior, and other observable attributes.</p>
         <p>knowing your phenotype is a powerful tool for personalized health management, allowing individuals to make informed decisions about their lifestyle, medical treatments, and overall well-being.</p>
-        <a href="https://rzp.io/l/oPJb73G7Fl" target="black">ASSESSMENT</a>
+        <a href="#form-container" target="black">ASSESSMENT</a>
         <br>
         <br>
         <h5>_</h5>
